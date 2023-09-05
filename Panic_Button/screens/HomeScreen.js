bc705eb8bbcfce384698b46e49d6acaca81fdc2f
Cristian Bevilaqua <104402733+Kemeker@users.noreply.github.com>
2023-09-05 16:05:13 -0300

    resolved problem

--- Panic_Button/screens/HomeScreen.js
+++ Panic_Button/screens/HomeScreen.js
@@ -3,10 +3,20 @@ import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
 import CustomButton from '../src/components/Button_Panic.js'
 
 function HomeScreen({ navigation }) {
+  const handleNavigateToContact = () => {
+    navigation.navigate('Contato');
+  };
+
   return (
     <View style={styles.container}>
       <Text>Botao do Panico</Text>
       <CustomButton />
+      <TouchableOpacity
+        style={styles.button}
+        onPress={handleNavigateToContact}
+      >
+        <Text>Ir para a Tela de Contato</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -17,7 +27,13 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
   },
+  button: {
+    backgroundColor: 'blue',
+    padding: 12,
+    borderRadius: 8,
+    alignItems: 'center',
+    marginTop: 20,
+  },
 });
 
 export default HomeScreen;
-
