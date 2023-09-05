e1c7ca5638d58a51585c3f299bb23add15a9d2b9
Cristian Bevilaqua <104402733+Kemeker@users.noreply.github.com>
2023-09-05 11:21:27 -0300

    add components: Button_Panic

--- Panic_Button/screens/HomeScreen.js
+++ Panic_Button/screens/HomeScreen.js
@@ -1,38 +1,23 @@
 import React from "react";
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import CustomButton from '../src/components/Button_Panic.js'
 
-function HomeScreen( {navigation}){
-    return (
-        <View style={StyleSheet.container}>
-            <Text>Botao do Panico</Text>
-            <TouchableOpacity
-            style={Styles.button}
-            onPress={()=> {
-                //Adicionar a logica do botao
-                }}
-                >
-                    <Text style={styles.buttonText}>Pressione Aqui</Text>
-                </TouchableOpacity>
-
-        </View>
-    );
+function HomeScreen({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Text>Botao do Panico</Text>
+      <CustomButton />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-    },
-    button: {
-      marginTop: 20,
-      padding: 10,
-      backgroundColor: 'blue',
-      borderRadius: 5,
-    },
-    buttonText: {
-      color: 'white',
-    },
-  });
-  
-  export default HomeScreen;
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
+
+export default HomeScreen;
+
---
+++ Panic_Button/src/components/Button_Panic.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+
+function CustomButton() {
+  return (
+    <TouchableOpacity style={styles.button}>
+      <View style={styles.back}></View>
+      <View style={styles.front}>
+        
+      </View>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  button: {
+    width: 60,
+    height: 50,
+    borderRadius: 100,
+    position: 'relative',
+    backgroundColor: 'transparent', // Ajuste a cor de fundo aqui
+    borderWidth: 1, // Ajuste a largura da borda aqui
+    borderColor: 'rgb(150, 50, 60)', // Ajuste a cor da borda aqui
+  },
+  back: {
+    backgroundColor: 'rgb(150, 50, 60)', // Cor de fundo do "back" do botão
+    borderRadius: 100,
+    position: 'absolute',
+    left: 0,
+    top: 0,
+    width: '100%',
+    height: '100%',
+  },
+  front: {
+    backgroundColor: 'transparent', // Cor de fundo do "front" do botão
+    borderRadius: 100,
+    position: 'absolute',
+    left: 0,
+    top: 0,
+    width: '100%',
+    height: '100%',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    fontSize: 12, // Ajuste o tamanho da fonte aqui
+    fontWeight: '600',
+    fontFamily: 'inherit',
+    color: 'rgb(150, 50, 60)', // Cor do texto
+    transform: [{ translateY: -15 }],
+  },
+  buttonText: {
+    color: 'white', // Cor do texto
+  },
+});
+
+export default CustomButton;
