5cb4eadd9e6adecd5b23d49f42785f2aa4523858
Kemeker <kemeker@gmail.com>
2023-09-08 15:30:59 -0300

    finalizado integraçao com db sqlite e testado ok

--- Panic_Button/Backend/rotas/contacts.js
+++ Panic_Button/Backend/rotas/contacts.js
@@ -1,57 +1,49 @@
 const express = require('express');
 const router = express.Router();
-const fs = require('fs'); // Importe o módulo 'fs' para ler o arquivo JSON
-const contacts = require('./contacts.json') //array criado para teste 
+const db = require('../db/DbContacts.db'); // Importe o objeto db do seu arquivo de banco de dados
+ 
 
-
+// rota para adicionar um contato ao banco de dados
 router.post('/contacts', (req, res)=>{
-    //receba os dados do coropo da solicitaçao  (req.body)
-    const newContact = req.body;
+   
+    const {name, phoneNumber} = req.body;
 
-    // Valide e insira os dados no banco de dados (ou lista)
-    // Por enquanto, vamos apenas adicionar o novo contato ao array
-    contacts.push(newContact);
+    db.run('INSERT INTO contacts (name, phoneNumber) VALUES (?, ?)', [name, phoneNumber], function(err) {
 
-    // Retorne a resposta adequada, por exemplo, um objeto JSON com o novo contato
-    res.status(201).json(newContact);
+       if (err){
+        return res.status(500).json({error: err.message});
+       } 
+       res.status(201).json({
+        id: this.lastID,
+        name,
+        phoneNumber
+       });
+    });
 });
 
-
-
-
+    
 // Rota para obter a lista de contatos
 router.get('/contacts', (req, res) => {
-  // Leia o arquivo JSON de contatos
-  fs.readFile('./rotas/contacts.json', 'utf8', (err, data) => {
-    if (err) {
-      console.error(err);
-      return res.status(500).json({ error: 'Erro ao ler o arquivo de contatos' });
-    }
-
-    // Parse o conteúdo do arquivo JSON para um objeto JavaScript
-    const contacts = JSON.parse(data);
-
-    // Retorna a lista de contatos em formato JSON
-    res.json(contacts);
+    db.all('SELECT * FROM contacts', [], (err, rows) => {
+      if (err) {
+        return res.status(500).json({ error: err.message });
+      }
+      res.status(200).json(rows);
+    });
   });
-});
+  
 
-
-router.delete('/contacts/:id', (req, res)=>{
+  // rota para deletar contato
+  router.delete('/contacts/:id', (req, res) => {
     const contactId = req.params.id;
-
-    // Encontre o índice do contato a ser excluído no array
-    const contactIndex = contacts.findIndex(contact => contact.id === contactId);
-
-    if (contactIndex === 0){
-     // Se o contato não for encontrado, retorne um status 404 (Não Encontrado)
-     res.status(404).json({error: 'Contato nao encontrado'});
-    } else {
-    // senao remova o contato do array
-      contacts.splice(contactIndex, 1);
-
-    res.status(204).send();
-    }
-});
+  
+    db.run('DELETE FROM contacts WHERE id = ?', contactId, (err) => {
+      if (err) {
+        return res.status(500).json({ error: err.message });
+      }
+      res.status(204).send();
+    });
+  });
+  
 
 module.exports = router;
